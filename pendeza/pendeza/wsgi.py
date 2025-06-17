db724b7bc4af50de14e4029f5d70becafbe52d0c
Khiisa Allan James <james21.khiisa@gmail.com>
2025-06-17 13:13:28 +0300

    Reversed the app name and project name

--- pendeza/pendeza/wsgi.py
+++ pendeza/pendeza/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for pendeza project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+from pathlib import Path
 
-from django.core.wsgi import get_wsgi_application
+# Add project root to Python path
+sys.path.append(str(Path(__file__).parent.parent))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pendeza.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
